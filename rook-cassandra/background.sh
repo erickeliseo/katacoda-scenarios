a273c47455b22756b225cdd03a9b19e2370daa81
Erick <erick.eliseo@gmail.com>
2018-10-02 22:05:12 -0600

    Changes

--- rook-cassandra/background.sh
+++ rook-cassandra/background.sh
@@ -1,1 +1,3 @@
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
+git clone https://github.com/rook/rook.git
+kubectl create -f rook/cluster/examples/kubernetes/ceph/operator.yaml
