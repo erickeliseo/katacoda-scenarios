ca0abfba306db7d2970418dbad78a9e2418b47a0
Erick <erick.eliseo@gmail.com>
2018-10-03 13:35:11 -0600

    Changes

--- rook-cassandra/foreground.sh
+++ rook-cassandra/foreground.sh
@@ -1,1 +1,7 @@
 launch.sh
+echo -e "\u001b[32mClone the Git Repository\u001b[m\r\n"
+git clone https://github.com/rook/rook.git
+git clone https://github.com/erickeliseo/files.git
+
+echo -e "\u001b[32mCambiando permisos al setup-rook_cluster.sh\u001b[m\r\n"
+chmod u+x files/setup-rook_cluster.sh
