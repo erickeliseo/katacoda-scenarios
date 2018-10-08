805735655081e57f909de6810a504187999d23ea
Erick <erick.eliseo@gmail.com>
2018-10-08 14:44:53 -0600

    Changes

--- rook-cassandra/foreground.sh
+++ rook-cassandra/foreground.sh
@@ -5,5 +5,4 @@ helm init
 chmod u+x files/setup-rook_cluster.sh
 echo -e "\u001b[32mWait for Kubernetes and Rook to be ready\u001b[m\r\n"
 kubectl create -f ~/rook/cluster/examples/kubernetes/ceph/operator.yaml
-sleep 5
 kubectl create -f ~/rook/cluster/examples/kubernetes/ceph/cluster.yaml
