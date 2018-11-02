3bc47861363d6cbce2dc96059e6af35abeb92924
Erick <erick.eliseo@gmail.com>
2018-11-02 08:54:53 -0600

    Changes

--- openshift-install/set-env.sh
+++
@@ -1,4 +0,0 @@
-/usr/local/bin/launch.sh
-mkdir -p /root/installation && cd ~/installation
-
-wget -c https://github.com/istio/istio/releases/download/1.0.3/istio-1.0.3-linux.tar.gz -P /root/installation
---
+++ openshift-practica2/set-env.sh
@@ -0,0 +1,6 @@
+/usr/local/bin/launch.sh
+mkdir -p /root/installation && cd ~/installation
+
+# -----------------
+git clone https://github.com/erickeliseo/katacoda-scenarios.git
+#wget -c https://github.com/istio/istio/releases/download/1.0.3/istio-1.0.3-linux.tar.gz -P /root/installation
